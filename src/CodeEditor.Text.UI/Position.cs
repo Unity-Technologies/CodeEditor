fabfa9438bc85b544210b8584113134417353f89
Mads Nyholm <mads@unity3d.com>
2012-11-15 10:59:28 +0100

    equal operator for Position

--- src/CodeEditor.Text.UI/Position.cs
+++ src/CodeEditor.Text.UI/Position.cs
@@ -14,6 +14,17 @@ namespace CodeEditor.Text.UI
 		public int Column {get{return _column;} set{_column = value;}}
 		public int Row {get{return _row;}  set{_row=value;}}
 
+		public static bool operator == (Position lhs, Position rhs)
+		{
+			return lhs.Row == rhs.Row && lhs.Column == rhs.Column;
+		}
+
+		public static bool operator !=(Position lhs, Position rhs)
+		{
+			return lhs.Column != rhs.Column || lhs.Row != rhs.Row;
+		}
+
+
 		public override string ToString ()
 		{
 			return string.Format ("(row {0}, column {1})", Row, Column);
