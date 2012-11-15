fdf77f14acf09fa5105a44a4d9234f889984f079
Mads Nyholm <mads@unity3d.com>
2012-11-15 13:32:03 +0100

    add selection test that now fails (issue with anchor and caret not on selection border)

--- src/CodeEditor.Text.UI.Tests/SelectionTest.cs
+++ src/CodeEditor.Text.UI.Tests/SelectionTest.cs
@@ -23,6 +23,34 @@ namespace CodeEditor.Text.UI
 		}
 
 		[Test]
+		public void TestThatCaretPosIsOnOneOfTheSelectionBorders ()
+		{
+			Position caretPos = new Position(5, 5);
+			Position[] anchorPos = new Position[] {
+				new Position (1,1), new Position (1,5), new Position (1, 10),
+				new Position (5,1), new Position (5,5), new Position (5, 10),
+				new Position (10,1), new Position (10,5), new Position (10, 10),
+			};
+			Selection selection = GetSelectionWithCaretPos(caretPos.Row, caretPos.Column);
+
+			for (int i=0; i<anchorPos.Length; i++)
+			{
+				selection.Anchor = anchorPos[i];
+				
+				bool caretIsOk = selection.BeginDrawPos == caretPos || selection.EndDrawPos == caretPos;
+				if (!caretIsOk)
+					System.Console.WriteLine("Error: Caret is NOT on selection border" + " \nData: " + selection + ", Index: " + i);
+
+				bool anchorIsOk = selection.BeginDrawPos == selection.Anchor || selection.EndDrawPos == selection.Anchor;
+				if (!anchorIsOk)
+					System.Console.WriteLine("Error: Anchor is NOT on selection border" + " \nData: " + selection + ", Index: " + i);
+
+				Assert.IsTrue(caretIsOk);
+				Assert.IsTrue(anchorIsOk);
+			}
+		}
+
+		[Test]
 		public void TestSelectionWhereCaretIsAfterAnchorOnSameRow()
 		{
 			Selection selection = GetSelectionWithCaretPos(5, 9);
