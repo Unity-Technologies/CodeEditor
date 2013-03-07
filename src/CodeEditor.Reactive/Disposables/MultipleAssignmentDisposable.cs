86b19170053753d4d7c03dd4fc4feba0205a01f2
Rodrigo B. de Oliveira <rbo@acm.org>
2013-03-07 17:02:14 -0300

    MultipleAssignmentDisposable

---
+++ src/CodeEditor.Reactive/Disposables/MultipleAssignmentDisposable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Disposables;
+
+namespace CodeEditor.Reactive.Disposables
+{
+	public class MultipleAssignmentDisposable : IDisposable
+	{
+		readonly MutableDisposable _disposable = new MutableDisposable();
+
+		public IDisposable Disposable
+		{
+			get { return _disposable.Disposable; }
+			set { _disposable.Disposable = value; }
+		}
+
+		public void Dispose()
+		{
+			_disposable.Dispose();
+		}
+	}
+}
