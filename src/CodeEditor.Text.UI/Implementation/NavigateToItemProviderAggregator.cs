27643270055c447119d8451a4007ab3525f9c575
Rodrigo B. de Oliveira <rbo@acm.org>
2013-03-07 17:00:06 -0300

    log search errors

--- src/CodeEditor.Text.UI/Implementation/NavigateToItemProviderAggregator.cs
+++ src/CodeEditor.Text.UI/Implementation/NavigateToItemProviderAggregator.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Linq;
 using CodeEditor.Composition;
+using CodeEditor.Logging;
 using CodeEditor.Reactive;
 
 namespace CodeEditor.Text.UI.Implementation
@@ -10,9 +12,21 @@ namespace CodeEditor.Text.UI.Implementation
 		[ImportMany]
 		public INavigateToItemProvider[] Providers { get; set; }
 
+		[Import]
+		public ILogger Logger { get; set; }
+
 		public IObservableX<INavigateToItem> Search(string filter)
 		{
-			return Providers.Select(provider => provider.Search(filter).Catch(ObservableX.Empty<INavigateToItem>())).Merge();
+			return Providers
+				.Select(provider =>
+					provider
+					.Search(filter)
+					.Catch((Exception exception) =>
+					{
+						Logger.LogError(exception);
+						return ObservableX.Empty<INavigateToItem>();
+					}))
+				.Merge();
 		}
 	}
 }
