3a948ce234bde56e655a61571a754257f4e3534f
Rodrigo B. de Oliveira <rbo@acm.org>
2013-04-22 19:01:38 -0300

    expose CompositeDisposable functionality through Disposable.Compose

--- src/CodeEditor.Reactive/Disposables/Disposable.cs
+++ src/CodeEditor.Reactive/Disposables/Disposable.cs
@@ -4,14 +4,19 @@ namespace CodeEditor.Reactive.Disposables
 {
 	public static class Disposable
 	{
+		public static IDisposable Empty
+		{
+			get { return System.Disposables.Disposable.Empty; }
+		}
+
 		public static IDisposable Create(Action dispose)
 		{
 			return System.Disposables.Disposable.Create(dispose);
 		}
 
-		public static IDisposable Empty
+		public static IDisposable Compose(params IDisposable[] disposables)
 		{
-			get { return System.Disposables.Disposable.Empty; }
+			return new System.Disposables.CompositeDisposable(disposables);
 		}
 	}
 }
