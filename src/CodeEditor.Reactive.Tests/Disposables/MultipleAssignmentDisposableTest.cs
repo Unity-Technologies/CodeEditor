2f7aef0d5e2de9c5dc4efdea00693e7fa04006d5
Rodrigo B. de Oliveira <rbo@acm.org>
2013-04-22 19:00:49 -0300

    fix MultipleAssignmentDisposable (semantics are not the same as MutableDisposable)

---
+++ src/CodeEditor.Reactive.Tests/Disposables/MultipleAssignmentDisposableTest.cs
@@ -0,0 +1,49 @@
+﻿using CodeEditor.Reactive.Disposables;
+using NUnit.Framework;
+
+namespace CodeEditor.Reactive.Tests.Disposables
+{
+	[TestFixture]
+	public class MultipleAssignmentDisposableTest
+	{
+		[Test]
+		public void DisposesUnderlyingDisposableOnce()
+		{
+			var disposed = 0;
+			var subject = new MultipleAssignmentDisposable {Disposable = Disposable.Create(() => ++disposed)};
+			Assert.AreEqual(0, disposed);
+			for (var i = 0; i < 2; ++i)
+			{
+				subject.Dispose();
+				Assert.AreEqual(1, disposed);
+			}
+		}
+
+		[Test]
+		public void ReplacingDisposableDoesNotCauseItToBeDisposed()
+		{
+			var first = 0;
+			var subject = new MultipleAssignmentDisposable {Disposable = Disposable.Create(() => ++first)};
+
+			var second = 0;
+			subject.Disposable = Disposable.Create(() => ++second);
+			Assert.AreEqual(0, first);
+			Assert.AreEqual(0, second);
+
+			subject.Dispose();
+			Assert.AreEqual(0, first);
+			Assert.AreEqual(1, second);
+		}
+
+		[Test]
+		public void SettingDisposableAfterDisposeCausesItToBeDisposed()
+		{
+			var subject = new MultipleAssignmentDisposable();
+			subject.Dispose();
+
+			var disposed = false;
+			subject.Disposable = Disposable.Create(() => disposed = true);
+			Assert.IsTrue(disposed);
+		}
+	}
+}
--- src/CodeEditor.Reactive/Disposables/MultipleAssignmentDisposable.cs
+++ src/CodeEditor.Reactive/Disposables/MultipleAssignmentDisposable.cs
@@ -1,21 +1,44 @@
 ﻿using System;
-using System.Disposables;
 
 namespace CodeEditor.Reactive.Disposables
 {
 	public class MultipleAssignmentDisposable : IDisposable
 	{
-		readonly MutableDisposable _disposable = new MutableDisposable();
+		readonly object _lock = new object();
+		bool _disposed;
+		IDisposable _disposable;
 
 		public IDisposable Disposable
 		{
-			get { return _disposable.Disposable; }
-			set { _disposable.Disposable = value; }
+			get
+			{
+				lock (_lock)
+					return _disposable;
+			}
+			set
+			{
+				lock (_lock)
+				{
+					if (_disposed)
+						value.Dispose();
+					else
+						_disposable = value;
+				}
+			}
 		}
 
 		public void Dispose()
 		{
-			_disposable.Dispose();
+			lock (_lock)
+			{
+				if (_disposed)
+					return;
+				_disposed = true;
+				if (_disposable == null)
+					return;
+				_disposable.Dispose();
+				_disposable = null;
+			}
 		}
 	}
 }
