871930d4d480f840e29315978db2f5d2f8e055b3
Mads Nyholm <mads@unity3d.com>
2013-04-23 09:57:22 +0200

    added GUISkinProvider

---
+++ src/CodeEditor.Text.UI.Unity.Editor/Implementation/UnityEditorGUISkinProvider .cs
@@ -0,0 +1,27 @@
+﻿using CodeEditor.Composition;
+using CodeEditor.Text.UI.Unity.Engine;
+using UnityEngine;
+
+namespace CodeEditor.Text.UI.Unity.Editor.Implementation
+{
+	[Export(typeof(IGUISkinProvider))]
+	class UnityEditorGUISkinProvider : IGUISkinProvider
+	{
+		GUISkin _guiSkin;
+
+		public GUISkin GetGUISkin()
+		{
+			if (_guiSkin == null)
+			{
+				string userSkinPath = "Assets/Editor/CodeEditor/CodeEditorSkin.guiskin";
+				_guiSkin = UnityEditor.AssetDatabase.LoadAssetAtPath(userSkinPath, typeof(GUISkin)) as GUISkin;
+				if (_guiSkin == null)
+				{
+					Debug.Log("CodeEditor skin not found! " + userSkinPath);
+					_guiSkin = GUI.skin;
+				}
+			}
+			return _guiSkin;
+		}
+	}
+}
---
+++ src/CodeEditor.Text.UI.Unity.Engine/IGUISkinProvider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CodeEditor.Text.UI.Unity.Engine
+{
+	public interface IGUISkinProvider
+	{
+		GUISkin GetGUISkin();
+	}
+}
